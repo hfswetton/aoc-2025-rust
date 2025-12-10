b7e35e62623cbd7688f835b63bb06ae7144a3af2
Henry Wetton <henry.wetton@phys.chem.ethz.ch>
2025-12-10 10:05:15 +0100

    Add timer to template

--- src/bin/_template.rs
+++ src/bin/_template.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
+use std::time::Instant;
 
 const OUTPUT_MESSAGE: &str = "Total";
 const INPUT_FILE: &str = "input/day_XX.txt";
@@ -11,6 +12,9 @@ fn calculate_result(lines: Lines<BufReader<File>>) -> Result<usize, ()> {
 fn main() {
     let file = File::open(INPUT_FILE).expect("unable to open file");
     let reader = BufReader::new(file);
+    let now = Instant::now();
     let result = calculate_result(reader.lines()).expect("error calculating result");
+    let elapsed_time = now.elapsed();
     println!("{OUTPUT_MESSAGE}: {result}");
+    println!("Result calculated in {} ms", elapsed_time.as_millis());
 }
